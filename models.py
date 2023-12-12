5bc689b8d1e2c7dd2902a62bbd4ec8e994e14590
yberube <yanmarkb@gmail.com>
2023-12-12 16:35:15 -0500

    added some more unittests

--- models.py
+++ models.py
@@ -1,25 +1,25 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy  # Import the SQLAlchemy module for working with databases
+from datetime import datetime  # Import the datetime module for working with dates and times
 
-db = SQLAlchemy()
+db = SQLAlchemy()  # Create an instance of the SQLAlchemy class to interact with the database
 
-class User(db.Model):
-    __tablename__ = "users"
+class User(db.Model):  # Define a User class that represents a table in the database
+    __tablename__ = "users"  # Set the name of the table in the database
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    image_url = db.Column(db.String(500), nullable=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Define a column for the user's ID
+    first_name = db.Column(db.String(50), nullable=False)  # Define a column for the user's first name
+    last_name = db.Column(db.String(50), nullable=False)  # Define a column for the user's last name
+    image_url = db.Column(db.String(500), nullable=True)  # Define a column for the user's image URL
 
-class Post(db.Model):
+class Post(db.Model):  # Define a Post class that represents a table in the database
     """Blog post."""
 
-    __tablename__ = "posts"
+    __tablename__ = "posts"  # Set the name of the table in the database
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.Text, nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Define a column for the post's ID
+    title = db.Column(db.Text, nullable=False)  # Define a column for the post's title
+    content = db.Column(db.Text, nullable=False)  # Define a column for the post's content
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Define a column for the post's creation date and time
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Define a column for the user ID associated with the post
 
-    user = db.relationship('User', backref='posts')
+    user = db.relationship('User', backref='posts')  # Define a relationship between the Post and User classes
--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
 import unittest
-from app import app, db, User
+from app import app, db, User, Post
 
 class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
         self.client = app.test_client()
         app.config['TESTING'] = True
+        self.ctx = app.app_context()
+        self.ctx.push()
+
 
     def test_home(self):
         # Sends a GET request to the home page and check if the response status code is 302 (redirect).
@@ -26,7 +29,34 @@ class FlaskTestCase(unittest.TestCase):
         # Sends a POST request to the new user page with form data and check if the response status code is 302 (redirect).
         response = self.client.post('/users/new', data={'first_name': 'Test', 'last_name': 'User', 'image_url': ''})
         self.assertEqual(response.status_code, 302)
+    
+    def test_new_post(self):
+        # This creates a new user
+        user = User(first_name='Test', last_name='User')
+        db.session.add(user)
+        db.session.commit()
 
+        # Then, send a POST request to the new post page with form data
+        # and check if the response status code is 302 (redirect).
+        response = self.client.post(f'/users/{user.id}/posts/new', data={'title': 'Test Post', 'content': 'Test Content'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_detail(self):
+        # This creates a new user and post
+        user = User(first_name='Test', last_name='User')
+        db.session.add(user)
+        db.session.commit()
+        post = Post(title='Test Post', content='Test Content', user_id=user.id)
+        db.session.add(post)
+        db.session.commit()
+
+        # Then, send a GET request to the post detail page
+        # and check if the response status code is 200 (success).
+        response = self.client.get(f'/posts/{post.id}')
+        self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        self.ctx.pop()
 if __name__ == '__main__':
     # Runs the unit tests.
     unittest.main()
